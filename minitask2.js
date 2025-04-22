e061caefb5e480d3d06f7230a75cc63d05601fdd
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-22 16:55:34 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,25 +1,32 @@
-//minitask 2 - membalik nama artis ke posisi depan
-
 const url = "https://gist.githubusercontent.com/fulsep/58daee79a699d0fe143da36bbc47e3d6/raw/dcc2e9c970af553011ba6d2d13331e68f5cf42ff/lagu.json"
-fetch(url)
-.then(res => res.json())
-.then(data=> {
-    // versi 1
-    let result = []
-    for(let i = 0; i < data.length; i++){
-        let index = data[i].indexOf(" - ")
-        const title = data[i].substring(0, index);
-        const artist = data[i].substring(index+3);
-        const titleArtist = `${artist} - ${title}`
-        result = [...result, titleArtist]
+const fs = require('fs').promises
+const path = require('path')
+
+// create file
+const createFile = async (name) => {
+    try {
+        const content = ""
+        await fs.writeFile(name, content)
+        console.log("write file success: ", name)
+    } catch (err) {
+        console.log(err)
     }
-    console.log(result);
+}
 
-    //versi 2
-    const song = data.map(item => {
-        const[title, artist] = item.split(" - ")
-        return `${artist} - ${title}`
-    })
-    console.log(song);
-})
 
+// create file
+const songFile = async () =>{
+    try {
+        const fetchData = await fetch(url)
+        const data = await fetchData.json()
+
+        const reverseName = data.map(item => item.split(" - ").reverse().join(" - "))
+
+        reverseName.forEach(music=>{
+            createFile(path.join("music", `${music}.mp3`))
+        })
+    } catch (err){
+        console.log(err)
+    }
+}
+songFile()  
