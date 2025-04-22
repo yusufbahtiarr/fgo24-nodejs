210bce10ab46baa6c1e8a8d148012d2ce5b5f752
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-22 16:06:16 +0700

    Add files via upload

---
+++ minitask2.js
@@ -0,0 +1,17 @@
+//minitask 2 - membalik nama artis ke posisi depan
+
+const url = "https://gist.githubusercontent.com/fulsep/58daee79a699d0fe143da36bbc47e3d6/raw/dcc2e9c970af553011ba6d2d13331e68f5cf42ff/lagu.json"
+fetch(url)
+.then(res => res.json())
+.then(data=> {
+    let result = []
+    for(let i = 0; i < data.length; i++){
+        let index = data[i].indexOf(" - ")
+        const title = data[i].substring(0, index);
+        const artist = data[i].substring(index+3);
+        const titleArtist = `${artist} - ${title}`
+        result = [...result, titleArtist]
+    }
+    console.log(result);
+})
+
